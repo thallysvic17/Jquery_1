a50c2f7854419ea1cede8e7196c19045b30de3dc
thallysvic17 <thallysvic17@gmail.com>
2023-01-27 10:57:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function(){
+
     $('header button').click(function(){
         $('form').slideDown();
     })
@@ -10,16 +11,23 @@ $(document).ready(function(){
 
     $('form').on('submit', function(e){
         e.preventDeFault();
+
         const endereconovaimagem = $('endereco-image-nova').val();
-        const novoitem =$('<li></li>');
+
+        const novoitem =$('<li style="display: none"></li>');
+
         $(`<img src="${endereconovaimagem}" /> `).appendTo(novoitem);
+
         $(`
             <div class="overlay-image-link">
-                <a href="${endereconovaimagem}"target="_blank" title="Ver imagem em tamanho real">
-                    ver imagem em tamaho real
+                <a href="${endereconovaimagem}" tittle='ver imagem em tamanho real' target="_blank">
+                ver imagem em tamanho real
                 </a>
             </div>
         `).appendTo(novoitem);
+
         $(novoitem).appendTo("ul");
+        $(novoitem).fadeIn(1000);
+        $('endereco-imagem-nova').val('')
     })
 })
