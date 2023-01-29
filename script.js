1dea548c5c86b48f13c2f40e95858a9d6ea3894e
thallysvic17 <thallysvic17@gmail.com>
2023-01-29 01:35:05 -0300

    commit 1

--- script.js
+++ script.js
@@ -1,33 +1,15 @@
-
 $(document).ready(function(){
-
-    $('header button').click(function(){
-        $('form').slideDown();
-    })
-
-    $('#cancelar').click(function(){
-        $('form').slideUp();
-    })
-
     $('form').on('submit', function(e){
-        e.preventDeFault();
+        e.preventDefault();
 
-        const endereconovaimagem = $('endereco-image-nova').val();
-
-        const novoitem =$('<li style="display: none"></li>');
-
-        $(`<img src="${endereconovaimagem}" /> `).appendTo(novoitem);
-
-        $(`
-            <div class="overlay-image-link">
-                <a href="${endereconovaimagem}" tittle='ver imagem em tamanho real' target="_blank">
-                ver imagem em tamanho real
-                </a>
-            </div>
-        `).appendTo(novoitem);
-
+        const listatarefa =$('#tarefas').val();
+        const novoitem = $('<li><li/>');
+        $(`<li>${listatarefa}</li>`).appendTo(novoitem);
         $(novoitem).appendTo("ul");
-        $(novoitem).fadeIn(1000);
-        $('endereco-imagem-nova').val('')
-    })
-})
+        $('li').click(function(){
+            $(this).addclass("riscado");
+        });    
+    });
+    $('#tarefa').val('');
+
+});
